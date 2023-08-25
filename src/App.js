30e78c96f0b9d74c9a18019d1b3dd0f2aff0141c
Laura Nesti <lauranesti@MacBook-Air-di-Laura.local>
2023-08-25 03:12:48 +0200

    first draft of app file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welocome to React</h1>
+      <hr />
+      <h3>July 25, 2023</h3>
+    </div>
+  );
+}
+
+export default App;
